5c31ab5492eca6f0bf330bf1f19bef74aae097f9
Arthur <lourencoarthur224@gmail.com>
2026-02-19 22:06:02 -0300

    Mecânica de Carimbar e verificar Qual carimbo foi usado em cada pacote e começo da adição das esteiras

--- source/scripts/enums.gd
+++ source/scripts/enums.gd
@@ -3,3 +3,9 @@ extends Node
 class_name Enum
 
 enum Characters {Player, Boss}
+
+enum MailKinds {Package, Letter}
+
+enum ObjectsInScene { Package1, Package2, Package3 }
+
+enum StampMarks {Good, Bad, Neuter, Invalid}
